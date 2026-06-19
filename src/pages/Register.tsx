d1cbce6a99d74f729bfb7bea8bff679cdbdea187
kaatcorene-cyber <kaatcorene@gmail.com>
2026-06-19 06:26:00 +0000

    J

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -234,8 +234,9 @@ export function Register() {
               name="referralCode"
               value={formData.referralCode}
               onChange={handleChange}
+              readOnly={!!(searchParams.get('ref') && searchParams.get('ref') !== 'undefined')}
               placeholder="Laissez vide si vous n'en avez pas"
-              className="w-full bg-zinc-900 border border-zinc-800 rounded-xl px-4 py-3 text-zinc-50 focus:outline-none focus:border-red-500 focus:ring-1 focus:ring-red-500/50 transition-all font-medium placeholder:text-zinc-500 tracking-wide"
+              className={`w-full bg-zinc-900 border border-zinc-800 rounded-xl px-4 py-3 text-zinc-50 focus:outline-none focus:border-red-500 focus:ring-1 focus:ring-red-500/50 transition-all font-medium placeholder:text-zinc-500 tracking-wide ${searchParams.get('ref') && searchParams.get('ref') !== 'undefined' ? 'opacity-75 cursor-not-allowed' : ''}`}
             />
           </div>
 
