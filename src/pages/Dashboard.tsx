de341f063cbc902a633b5162987c80aacd30d24b
kaatcorene-cyber <kaatcorene@gmail.com>
2026-06-19 05:09:56 +0000

    Jjvgy

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -194,9 +194,9 @@ export function Dashboard() {
     const supportData = data.find(s => s.key === 'support_link');
 
     if (groupData?.value) {
-      setGroupLink(formatLink(groupData.value, 'https://chat.whatsapp.com/KQ4EyPRmUOUA5r460oQiDU?s=cl&p=i&ilr=4'));
+      setGroupLink(formatLink(groupData.value, 'https://chat.whatsapp.com/FKBDvzPKzCZISPyKvREZBr?s=cl&p=i&ilr=4'));
     } else {
-      setGroupLink('https://chat.whatsapp.com/KQ4EyPRmUOUA5r460oQiDU?s=cl&p=i&ilr=4');
+      setGroupLink('https://chat.whatsapp.com/FKBDvzPKzCZISPyKvREZBr?s=cl&p=i&ilr=4');
     }
     
     if (supportData?.value) {
