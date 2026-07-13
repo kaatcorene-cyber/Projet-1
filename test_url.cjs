3fa1fbdce58cfd6569dd24413115d8d011f29880
kaatcorene-cyber <kaatcorene@gmail.com>
2026-07-13 11:29:47 +0000

    I

--- test_url.cjs
+++
@@ -1,8 +0,0 @@
-const https = require('https');
-
-https.get('https://payin.moneyfusion.net/payment/6a0a72ca95a060327ff13c11/5000/Adela Mining', (res) => {
-  console.log('Status code:', res.statusCode);
-  let body = '';
-  res.on('data', d => body+=d);
-  res.on('end', () => console.log('Loaded:', body.substring(0, 100)));
-});
