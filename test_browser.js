86543fab33110d5762db2f4d5f580f089b893b69
kaatcorene-cyber <kaatcorene@gmail.com>
2026-08-11 07:24:12 +0000

    D

--- test_browser.js
+++ test_browser.js
@@ -4,16 +4,18 @@ import puppeteer from 'puppeteer';
   const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
   const page = await browser.newPage();
   
-  page.on('console', msg => console.log('PAGE LOG:', msg.text()));
-  page.on('pageerror', error => console.log('PAGE ERROR:', error.message));
-  page.on('response', response => {
-    if (response.status() >= 400) {
-      console.log('FAILED RESOURCE:', response.url(), response.status());
-    }
-  });
-  
   await page.goto('http://127.0.0.1:3000/login');
   await new Promise(r => setTimeout(r, 2000));
+  
+  const rect = await page.evaluate(() => {
+    const img = document.querySelector('img');
+    const form = document.querySelector('form');
+    return {
+      img: img ? img.getBoundingClientRect() : null,
+      form: form ? form.getBoundingClientRect() : null,
+    };
+  });
+  console.log(JSON.stringify(rect, null, 2));
   
   await browser.close();
 })();
